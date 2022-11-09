f8b53693eadac6846e152bc6515195d9b957246f
Nagaprasad Naik <naikanaga99@gmail.com>
2022-11-09 15:51:51 -0800

    index-style-js file

---
+++ test.js
@@ -0,0 +1,25 @@
+// const student ={ 
+//     fname : "Nagaprasad",
+//     lname : "Naik",
+
+//     set setName(name){
+//         return this.fname = name;
+//     },
+//     get getName(){
+//         return this.fname;
+//     }
+// }
+
+// console.log(student.getName);
+// student.setName = 'vivek';
+// console.log(student.getName);
+
+
+// let fname = prompt("enter name");
+// console.log(fname);
+// function fun(name){
+//     this.name = name;
+//     console.log(this);
+// }
+// var nm = new fun("val");
+// console.log(nm.name);
